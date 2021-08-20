7d5b623149d69bb9160c052dabf538efee9bb1b6
mpgxc <mpgx5.c@gmail.com>
2021-08-20 04:42:29 -0300

    feat: implement fake/in-memory repository

---
+++ src/domain/repositories/ILinksRepository.ts
@@ -0,0 +1,8 @@
+import { CreateShortenedLinkDTO } from "../useCases/CreateShortenedLink/CreateShortenedLinkDTO";
+
+interface ILinksRepository {
+  save(data: CreateShortenedLinkDTO): Promise<void>;
+  findByCode(id: string): Promise<CreateShortenedLinkDTO | null>;
+}
+
+export { ILinksRepository }
---
+++ src/domain/repositories/memory/LinksRepository.ts
@@ -0,0 +1,35 @@
+import { CreateShortenedLinkDTO } from "../../useCases/CreateShortenedLink/CreateShortenedLinkDTO";
+import { ILinksRepository } from "../ILinksRepository";
+
+class LinksRepository implements ILinksRepository {
+    private links: CreateShortenedLinkDTO[];
+
+    constructor() {
+        this.links = [];
+    }
+
+    async findByCode(id: string): Promise<CreateShortenedLinkDTO | null> {
+        const links = this.links.find(link => link.unique_id === id);
+
+        if (!links) {
+            return null;
+        }
+
+        return links;
+    }
+
+    async save({
+        original_url,
+        qrcode_url,
+        short_url,
+        unique_id, }: CreateShortenedLinkDTO): Promise<void> {
+        this.links.push({
+            original_url,
+            qrcode_url,
+            short_url,
+            unique_id,
+        })
+    }
+}
+
+export { LinksRepository };
