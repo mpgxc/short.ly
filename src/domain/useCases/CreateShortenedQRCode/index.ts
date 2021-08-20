3d5aba400f0805f71bc11ebec80f7902258a3b97
mpgxc <mpgx5.c@gmail.com>
2021-08-20 04:45:01 -0300

    feat: added use case to redirect a short link

---
+++ src/domain/useCases/CreateShortenedQRCode/index.ts
@@ -0,0 +1,5 @@
+import { CreateShortenedQRCodeController } from "./CreateShortenedQRCodeController";
+
+const createShortenedQRCodeController = new CreateShortenedQRCodeController ()
+
+export { createShortenedQRCodeController }
