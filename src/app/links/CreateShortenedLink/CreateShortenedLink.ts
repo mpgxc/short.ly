94b525a1aefd5f96537d87d0e958a7a8aad272b1
mpgxc <mpgx5.c@gmail.com>
2021-09-13 00:42:26 -0300

    refactor: added check if customer exists!

--- src/app/links/CreateShortenedLink/CreateShortenedLink.ts
+++ src/app/links/CreateShortenedLink/CreateShortenedLink.ts
@@ -1,11 +1,17 @@
 import { inject, injectable } from 'tsyringe';
 
 import { InjectableModules } from '@app/container/InjectableModules';
+import { ICustomerRepository } from '@domain/customer/ICustomerRepository';
 import { ILinksRepository, Links, URL } from '@domain/links';
 import { ILinkShortenerProvider } from '@infra/providers/LinkShortener';
 
 import { CreateShortenedLinkView } from './CreateShortenedLinkView';
 import { ResponseShortenedLink } from './ResponseShortenedLink';
+
+type ShortenedLinkRequest = {
+    url: string;
+    customerId: string;
+};
 
 @injectable()
 class CreateShortenedLink {
@@ -13,12 +19,32 @@ class CreateShortenedLink {
         @inject(InjectableModules.LINKS_REPOSITORY)
         private readonly linksRepository: ILinksRepository,
 
+        @inject(InjectableModules.CUSTOMER_REPOSITORY)
+        private readonly customersRepository: ICustomerRepository,
+
         @inject(InjectableModules.LINK_SHORTENER_PROVIDER)
         private readonly linkShortenerProvider: ILinkShortenerProvider,
     ) {}
 
-    async run(url: string): Promise<ResponseShortenedLink> {
-        const urlAlreadyExists = await this.linksRepository.findByURL(url);
+    async run({
+        customerId,
+        url,
+    }: ShortenedLinkRequest): Promise<ResponseShortenedLink> {
+        const customer = await this.customersRepository.findById(customerId);
+
+        if (!customer) {
+            throw new Error('Customer not found');
+        }
+
+        const instanceURL = URL.build(url);
+
+        if (!instanceURL.value) {
+            throw new Error('Invalid URL!');
+        }
+
+        const urlAlreadyExists = await this.linksRepository.findByURL(
+            instanceURL.value,
+        );
 
         if (urlAlreadyExists) {
             throw new Error(`URL already exists!`);
@@ -28,7 +54,8 @@ class CreateShortenedLink {
 
         const links = Links.build({
             token,
-            url: URL.build(url),
+            url: instanceURL,
+            customerId,
         });
 
         await this.linksRepository.save(links);
